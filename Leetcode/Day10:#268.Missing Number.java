8e2459762e8789513b89ec080d1e4a7c228aa364
kRamu581 <kanamramu18@gmail.com>
2025-05-04 09:51:18 +0530

    Create Day10:#268.Missing Number.java

---
+++ Leetcode/Day10:#268.Missing Number.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int range = nums.length;
+        int actualSum =  (range * (range + 1))/2;
+        int currSum = 0 ;
+        for(int i=0;i<nums.length;i++){
+            currSum = currSum + nums[i];
+        }
+        int ans = actualSum - currSum ;
+        return ans ;
+    }
+}
+
+
+
+➡️➡️➡️✌️ method:
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i <= n; i++) {
+            boolean found = false;
+            for (int j = 0; j < n; j++) {
+                if (nums[j] == i) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                return i; // Missing number found
+            }
+        }
+        return -1; // This should never happen
+    }
+}
