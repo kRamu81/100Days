9ef0c2b9467ca9944531462cbacbc32046c8a227
kRamu581 <kanamramu18@gmail.com>
2025-05-05 07:58:07 +0530

    Create Day12:#977. Squares of a Sorted Array.java

---
+++ Leetcode/Day12:#977. Squares of a Sorted Array.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+
+        int[] ans = new int[nums.length];
+        int start = 0 ;
+        int end = nums.length-1 ;
+        int ptr = ans.length-1 ;
+
+        while(start<=end){
+            int ss = nums[start] * nums[start];
+            int es = nums[end] * nums[end];
+            if(ss>es){
+                ans[ptr] = ss;
+                start++;
+            }else{
+                ans[ptr] = es ;
+                end--;
+            }
+            ptr--;
+        }
+        return ans ;
+    }
+}
+
+➡️➡️➡️✌️
+
+
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int n= nums.length;
+        int[] ans=new int[n];
+        for(int i=0;i<n;i++){
+            ans[i] = nums[i]*nums[i];
+        }
+        Arrays.sort(ans);
+        return ans;
+    }
+}
